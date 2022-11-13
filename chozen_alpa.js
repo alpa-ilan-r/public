8027849a623df3c0ea940ea7c29190ca7b06c293
alpa-ilan-r <116561142+alpa-ilan-r@users.noreply.github.com>
2022-11-13 13:27:09 +0200

    Update chozen_alpa.js
    JQuery on document ready added

--- chozen_alpa.js
+++ chozen_alpa.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.info('chozen_alpa.js loaded');
 }, false);
+jQuery( document ).ready(function() {
+    console.info('chozen_alpa.js loaded');
+});
