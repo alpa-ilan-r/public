7bdf162dde66aad21d2b652823e82f8fda71cee8
alpa-ilan-r <116561142+alpa-ilan-r@users.noreply.github.com>
2022-11-22 12:09:27 +0200

    BF 2022 Video no sound

--- chozen_alpa.js
+++ chozen_alpa.js
@@ -4,3 +4,17 @@ document.addEventListener('DOMContentLoaded', function () {
 jQuery( document ).ready(function() {
     console.info('chozen_alpa.js loaded');
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (document.location.href.includes('/checkout')) {
+        var coupon_code_input = document.getElementById('coupon_code');
+        coupon_code_input.addEventListener("keydown", (event) => {
+            if (event.keyCode === 189 || event.keyCode === 109) {
+                event.preventDefault();
+            }
+        });
+        // coupon_code_input.addEventListener('blur', function() {
+        //     coupon_code_input.value = coupon_code_input.value.replaceAll("-", "");
+        // });
+    }
+}, false);
